ef43a543fc56d078f539cec29e526cdc168220e5
hs.moon <hs.moon@wiz-factory.com>
2025-02-26 13:38:49 +0900

    2025-02-26 Reducer를 이용해 2개이상의 상태를 제어

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,26 @@ import "./App.css";
 // 상태의 타입 정의
 interface State {
   count: number;
+  isOn: boolean;
 }
 
 // 액션의 타입 정의
 type Action = 
   | { type: "increment" }
   | { type: "decrement" } 
-  | { type: "reset" };
+  | { type: "reset" }
+  | { type: "toggle" };
 
 // 초기 상태
-const initialState: State = { count: 0 };
+const initialState: State = { count: 0, isOn: false };
 
 // 리듀서 함수 정의
 function reducer(state: State, action: Action): State {
   switch (action.type) {
-    case "increment": return { count: state.count + 1 };
-    case "decrement": return { count: state.count - 1 };
-    case "reset": return initialState;
+    case "increment": return { ...state, count: state.count + 1 };
+    case "decrement": return { ...state, count: state.count - 1 };
+    case "reset":     return initialState;
+    case "toggle":    return { ...state, isOn: !state.isOn };
     default:
       throw new Error("Unhadled action type");
   }
@@ -31,10 +34,16 @@ const App: React.FC = () => {
 
   return (
     <>
+      <h2>카운터와 토글</h2>
       <p>Count: {state.count}</p>
       <button onClick={() => dispatch({ type: "increment" })}>+</button>
       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+
+      <p>토글 상태: { state.isOn ? "ON" : "OFF" }</p>
+      <button onClick={() => dispatch({ type: "toggle" })}>
+        {state.isOn ? "Turn Off" : "Turn On"}
+      </button>
     </>
   );
 };
